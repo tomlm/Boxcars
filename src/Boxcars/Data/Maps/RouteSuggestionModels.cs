b31660acae1ff18eb486bbe0a6436d89c0025906
Tom Laird-McConnell <thermous@iciclecreek.com>
2026-03-11 12:20:59 -0700

    clean up style start

--- src/Boxcars/Data/Maps/RouteSuggestionModels.cs
+++ src/Boxcars/Data/Maps/RouteSuggestionModels.cs
@@ -104,23 +104,9 @@ public sealed class RouteSuggestionSegmentOverlay
     public string BorderColor => IsThisTurn
         ? OwnershipCategory switch
         {
-            RailroadOwnershipCategory.Friendly => GetContrastColor(PlayerColor),
+            RailroadOwnershipCategory.Friendly => ColorUtilities.GetContrastColor(PlayerColor),
             RailroadOwnershipCategory.Unfriendly => PlayerColor,
             _ => PlayerColor
         }
         : "#FFFFFF";
-
-    private static string GetContrastColor(string hexColor)
-    {
-        if (string.IsNullOrWhiteSpace(hexColor) || hexColor.Length < 7 || hexColor[0] != '#')
-        {
-            return "#FFFFFF";
-        }
-
-        var r = Convert.ToInt32(hexColor.Substring(1, 2), 16);
-        var g = Convert.ToInt32(hexColor.Substring(3, 2), 16);
-        var b = Convert.ToInt32(hexColor.Substring(5, 2), 16);
-        var luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255.0;
-        return luminance > 0.5 ? "#000000" : "#FFFFFF";
-    }
 }
